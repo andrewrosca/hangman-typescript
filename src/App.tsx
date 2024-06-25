1f9d9ee4ec34c43bb7e23c20f75c316959ddf47d
Andrew Rosca <andrewrosca7@gmail.com>
2024-06-25 11:44:52 -0700

    Added functionality for adding word to display upon keyboard click and functionality for hangman to appear on wrong clicked letters

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import words from "./wordList.json";
 import HangmanDrawing from './HangmanDrawing';
 import HangmanWord from './HangmanWord';
@@ -11,6 +11,31 @@ function App() {
   });
 
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  const incorrectLetters = guessedLetters.filter(
+    letter => !wordToGuess.includes(letter)
+  );
+
+  const addGuessedLetter = useCallback((letter: string) => {
+    if (guessedLetters.includes(letter)) return;
+    setGuessedLetters(currentLetters => [...currentLetters, letter]);
+  }, [guessedLetters])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (!key.match(/^[a-z]$/)) return;
+
+      e.preventDefault();
+      addGuessedLetter(key);
+
+    }
+    document.addEventListener("keypress", handler);
+    return () => {
+      document.removeEventListener("keypress", handler);
+    }
+
+  }, [guessedLetters]);
 
   console.log(wordToGuess);
   return <div style={{
@@ -25,8 +50,8 @@ function App() {
     <div style={{
       fontSize: "2rem", textAlign: "center"}}>LoseWin
     </div>
-    <HangmanDrawing />
-    <HangmanWord />
+    <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+    <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
     <div style={{alignSelf: "stretch"}}>
       <Keyboard />
     </div>
--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,6 +1,9 @@
-function HangmanWord () {
-    const word = "test";
-    const guessedLetters = ['e', "t"]
+type HangmanWordProps = {
+    guessedLetters: string[];
+    wordToGuess: string;
+}
+
+function HangmanWord ({ guessedLetters, wordToGuess }: HangmanWordProps) {
     return (
         <div 
             style={{ 
@@ -12,7 +15,7 @@ function HangmanWord () {
                 fontFamily: "monospace"
             }}
         >
-        {word.split("").map((letter, index) => (
+        {wordToGuess.split("").map((letter, index) => (
             <span style={{borderBottom: ".1em solid black"}} key={index}>
                 <span style={{
                     visibility: guessedLetters.includes(letter)
